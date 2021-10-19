22e8325b43d1b7010891c358e3f1b86592865af9
Enzo Di Tizio <enzodtz@gmail.com>
2021-10-19 16:37:37 -0300

    Preventing error for compatibility improvements
    This error happened to me (https://stackoverflow.com/questions/1841565/valueerror-invalid-literal-for-int-with-base-10) so I'm just fixing it and maybe other can use it also.

--- Tensorflow/scripts/generate_tfrecord.py
+++ Tensorflow/scripts/generate_tfrecord.py
@@ -85,10 +85,10 @@ def xml_to_csv(path):
                      int(root.find('size')[0].text),
                      int(root.find('size')[1].text),
                      member[0].text,
-                     int(member[4][0].text),
-                     int(member[4][1].text),
-                     int(member[4][2].text),
-                     int(member[4][3].text)
+                     int(float(member[4][0].text)),
+                     int(float(member[4][1].text)),
+                     int(float(member[4][2].text)),
+                     int(float(member[4][3].text))
                      )
             xml_list.append(value)
     column_name = ['filename', 'width', 'height',
