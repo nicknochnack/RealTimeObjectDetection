63428b81aa0d27436e002d5f0deb20eb835c96c6
Syed Nahin Hossain <50939853+NAHIN-JZS@users.noreply.github.com>
2022-09-01 14:33:12 +0600

    Update generate_tfrecord.py
    In create_tf_example function, there is a bug. xmins->row['min'], xmaxs->row['ymin'] (I don't know why), ymins->row['xmax'], ymaxs -> row['ymax']...

--- Tensorflow/scripts/generate_tfrecord.py
+++ Tensorflow/scripts/generate_tfrecord.py
@@ -125,8 +125,8 @@ def create_tf_example(group, path):
 
     for index, row in group.object.iterrows():
         xmins.append(row['xmin'] / width)
-        xmaxs.append(row['xmax'] / width)
-        ymins.append(row['ymin'] / height)
+        xmaxs.append(row['ymin'] / width)
+        ymins.append(row['xmax'] / height)
         ymaxs.append(row['ymax'] / height)
         classes_text.append(row['class'].encode('utf8'))
         classes.append(class_text_to_int(row['class']))
